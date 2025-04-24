9ba8ac4e8a4dba784d64696ed23df5f96ec80756
Kevin Amexo <bangitskev@gmail.com>
2025-04-24 13:02:08 +0100

    feat(layout): add header component with initial styling using shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-// import { useState } from 'react'
-
 import "./App.css";
 import { Button } from "./components/ui/button";
+import Header from "./components/ui/Header";
 import RoutineFormDialog from "./components/ui/RoutineFormDialog";
 import { useAppSelector } from "./hooks/reduxHooks";
 
@@ -9,19 +8,22 @@ function App() {
   const { routines } = useAppSelector((state) => state.routines);
 
   return (
-    <main>
-      <Button>RoutineSync</Button>
-      <RoutineFormDialog />
-      {routines &&
-        routines.map((routine) => (
-          <div key={routine.id} className="flex flex-col gap-2">
-            <h2>{routine.name}</h2>
-            <p>{routine.description}</p>
-            <p>{routine.colour}</p>
-            <p>{routine.days.join(", ")}</p>
-          </div>
-        ))}
-    </main>
+    <div className="">
+      <Header />
+      <main className="container mx-auto max-w-5xl">
+        <Button>RoutineSync</Button>
+        <RoutineFormDialog />
+        {routines &&
+          routines.map((routine) => (
+            <div key={routine.id} className="flex flex-col gap-2">
+              <h2>{routine.name}</h2>
+              <p>{routine.description}</p>
+              <p>{routine.colour}</p>
+              <p>{routine.days.join(", ")}</p>
+            </div>
+          ))}
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button } from "./button";
+
+const Header = () => {
+  return (
+    <header className="container py-2">
+      <div className="mx-auto max-w-5xl px-4 py-3 flex items-center justify-between">
+        <div className="">
+          <h1 className="text-3xl font-bold tracking-tight bg-gradient-to-r from-primary via-violet-500 to-primary bg-clip-text text-transparent">
+            RoutineSync
+          </h1>
+          <h2 className="text-slate-400 mt-1 ">
+            Build Consistency with weekly routines
+          </h2>
+        </div>
+
+        {/* Right-side actions (add nav/profile later) */}
+        <div className="flex items-center gap-2">
+          <Button className="justify-center whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 flex items-center gap-1 rounded-full shadow-md bg-gradient-to-r from-primary to-violet-500 hover:shadow-lg transition-all">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-circle-plus h-4 w-4"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <path d="M8 12h8"></path>
+              <path d="M12 8v8"></path>
+            </svg>
+            <span className="">New Routine</span>
+          </Button>
+        </div>
+      </div>
+      {/* <Separator /> */}
+    </header>
+  );
+};
+
+export default Header;
